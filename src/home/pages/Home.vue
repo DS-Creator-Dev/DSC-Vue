39f2870b9a3c65e5de96099c96b544476146ada3
BowersIndustry <Bowersindustry.bwrs@gmail.com>
2022-12-23 07:48:33 -0600

    Electron API

--- src/home/pages/Home.vue
+++ src/home/pages/Home.vue
@@ -3,12 +3,12 @@
         <div class="h-full flex items-center">
             <div>
                 <div class="flex flex-col items-center" style="text-align:center;">
-                    <img src="../../../public/icon_x7.png" class="w-32 h-32"/>
+                    <img src="../../../public/icon_x32.png" class="w-32 h-32"/>
                     <p class="font-bold text-3xl">DS <span class="text-teal-500">Creator</span></p>
                 </div>
                 <div class="flex flex-row gap-1 mt-2 text-white">
-                    <button @click="open_project()" class="bg-teal-500 hover:bg-teal-400 transition-all p-1 rounded">Load Project</button>
-                    <button class="bg-zinc-500 hover:bg-zinc-400 transition-all p-1 rounded">New Project</button>
+                    <button @click="open_project()" class="bg-teal-500 hover:bg-teal-400 active:bg-teal-600 transition-all p-1 rounded">Load Project</button>
+                    <button class="bg-zinc-500 hover:bg-zinc-400 active:bg-zinc-600 transition-all p-1 rounded">New Project</button>
                 </div>
             </div>
         </div>
@@ -16,15 +16,7 @@
     
 </template>
 <script setup lang="ts">
-import { onMounted } from 'vue';
-onMounted(async()=>{
-    
-})
-
-async function open_project(){
-    var path = await window.electron.open_project();
-    console.log(path)
-}
+import { open_project } from '../../api/electron';
 </script>
 <style scoped lang="css">
 </style>
