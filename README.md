e776167380c2cd79c48b977a38317aa0960b892d
BowersIndustry <Bowersindustry.bwrs@gmail.com>
2022-12-23 07:10:35 -0600

    Added some stuff to the home page.
    Also, the load project button will make a popup window if running in Electron

--- README.md
+++ README.md
@@ -5,4 +5,5 @@
 Open the CMD or Terminal in the root directory of the project. Run `npm i` if you haven't already. Next, run `npm run build` this will generate the code that can be used by Electron. Then, go into the `index.html` file in the `dist` folder. Add a `.` before `/assets/` in line 5. Do the same modification in lines 8 & 9. Now back in your CMD or Terminal instance, run `npm start`. This will start up Electron.
 
 ## Note:
-When trying to access a file from `/public` use the full path to get there (example: `../../public/image.png`) not `/image.png` like you would normally.
+- When trying to access a file from `/public` use the full path to get there (example: `../../public/image.png`) not `/image.png` like you would normally. This allows Electron to actually load the files.
+- When running in server mode (`npm run dev`) you will not be able to use Electron functions. But it is great for UI design.
--- src/home/pages/Home.vue
+++ src/home/pages/Home.vue
@@ -1,13 +1,30 @@
 <template>
-    <img src="../../../public/icon_x7.png"/>
-    <input type="file" />
+    <div class="w-full h-screen flex justify-center font-title">
+        <div class="h-full flex items-center">
+            <div>
+                <div class="flex flex-col items-center" style="text-align:center;">
+                    <img src="../../../public/icon_x7.png" class="w-32 h-32"/>
+                    <p class="font-bold text-3xl">DS <span class="text-teal-500">Creator</span></p>
+                </div>
+                <div class="flex flex-row gap-1 mt-2 text-white">
+                    <button @click="open_project()" class="bg-teal-500 hover:bg-teal-400 transition-all p-1 rounded">Load Project</button>
+                    <button class="bg-zinc-500 hover:bg-zinc-400 transition-all p-1 rounded">New Project</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    
 </template>
 <script setup lang="ts">
 import { onMounted } from 'vue';
 onMounted(async()=>{
+    
+})
+
+async function open_project(){
     var path = await window.electron.open_project();
     console.log(path)
-})
+}
 </script>
 <style scoped lang="css">
 </style>
